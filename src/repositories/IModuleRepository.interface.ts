b0379acad3fc0a2a66fbbdcdbd2444b1df9445e4
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-10-26 14:54:44 -0300

    feat(modules): add interfaces and types for modules

---
+++ src/repositories/IModuleRepository.interface.ts
@@ -0,0 +1,51 @@
+export interface Module {
+  id: number
+  name: string
+  slug: string
+  order: number
+  description: string
+  coverUrl: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface ModuleWhereInputs {
+  name?: {
+    contains?: string
+    mode?: 'insensitive' | 'default'
+  }
+}
+
+export interface CreateModuleAttributes {
+  name: string
+  slug: string
+  order: number
+  description: string
+  coverUrl: string
+}
+
+export interface UpdateModuleAttributes {
+  name?: string
+  slug?: string
+  order?: number
+  description?: string
+  coverUrl?: string
+}
+
+export interface FindModulesParams {
+  where?: ModuleWhereInputs
+  sortBy?: 'name' | 'createdAt'
+  order?: 'asc' | 'desc'
+  skip?: number
+  take?: number
+}
+
+export interface IModuleRepository {
+  find(params: FindModulesParams): Promise<Module[]>
+  findById(id: number): Promise<Module | null>
+  findBySlug(slug: string): Promise<Module | null>
+  create(attributes: CreateModuleAttributes): Promise<Module>
+  update(id: number, attributes: UpdateModuleAttributes): Promise<Module>
+  delete(id: number): Promise<void>
+  count(where: ModuleWhereInputs): Promise<number>
+}
