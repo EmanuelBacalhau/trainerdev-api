f720d4cc139fe60c498e62978d33769946ee166c
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-10-26 14:58:32 -0300

    feat(module): integrate Module modules into application setup

--- src/container.ts
+++ src/container.ts
@@ -1,7 +1,10 @@
+import { ModuleController } from './controllers/Module.controller'
 import { TrackController } from './controllers/Track.controller'
 import { UserController } from './controllers/User.controller'
+import { PrismaModuleRepository } from './repositories/prisma/PrismaModule.repository'
 import { PrismaTrackRepository } from './repositories/prisma/PrismaTrack.repository'
 import { PrismaUserRepository } from './repositories/prisma/PrismaUser.repository'
+import { ModuleUseCase } from './use-cases/Module.use-case'
 import { TrackUseCase } from './use-cases/Track.use-case'
 import { UserUseCase } from './use-cases/User.use-case'
 
@@ -12,3 +15,7 @@ export const userController = new UserController(userUseCase)
 const trackRepository = new PrismaTrackRepository()
 const trackUseCase = new TrackUseCase(trackRepository)
 export const trackController = new TrackController(trackUseCase)
+
+const moduleRepository = new PrismaModuleRepository()
+const moduleUseCase = new ModuleUseCase(moduleRepository)
+export const moduleController = new ModuleController(moduleUseCase)
