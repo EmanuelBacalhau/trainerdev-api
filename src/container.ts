107e532d03b9250e317514ccd6e56b69fa2b9e85
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-10-25 22:06:36 -0300

    feat(track): integrate Track module into application setup

--- src/container.ts
+++ src/container.ts
@@ -1,7 +1,14 @@
+import { TrackController } from './controllers/Track.controller'
 import { UserController } from './controllers/User.controller'
+import { PrismaTrackRepository } from './repositories/prisma/PrismaTrack.repository'
 import { PrismaUserRepository } from './repositories/prisma/PrismaUser.repository'
+import { TrackUseCase } from './use-cases/Track.use-case'
 import { UserUseCase } from './use-cases/User.use-case'
 
 const userRepository = new PrismaUserRepository()
 const userUseCase = new UserUseCase(userRepository)
 export const userController = new UserController(userUseCase)
+
+const trackRepository = new PrismaTrackRepository()
+const trackUseCase = new TrackUseCase(trackRepository)
+export const trackController = new TrackController(trackUseCase)
