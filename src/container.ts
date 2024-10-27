b37b367b43e1a81cf587347fa498d85308207e12
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-10-27 11:16:31 -0300

    feat(lesson): integrate Lesson modules into application setup

--- src/container.ts
+++ src/container.ts
@@ -1,9 +1,12 @@
+import { LessonController } from './controllers/Lesson.controller'
 import { ModuleController } from './controllers/Module.controller'
 import { TrackController } from './controllers/Track.controller'
 import { UserController } from './controllers/User.controller'
+import { PrismaLessonRepository } from './repositories/prisma/PrismaLesson.repository'
 import { PrismaModuleRepository } from './repositories/prisma/PrismaModule.repository'
 import { PrismaTrackRepository } from './repositories/prisma/PrismaTrack.repository'
 import { PrismaUserRepository } from './repositories/prisma/PrismaUser.repository'
+import { LessonUseCase } from './use-cases/Lesson.use-case'
 import { ModuleUseCase } from './use-cases/Module.use-case'
 import { TrackUseCase } from './use-cases/Track.use-case'
 import { UserUseCase } from './use-cases/User.use-case'
@@ -19,3 +22,7 @@ export const trackController = new TrackController(trackUseCase)
 const moduleRepository = new PrismaModuleRepository()
 const moduleUseCase = new ModuleUseCase(moduleRepository)
 export const moduleController = new ModuleController(moduleUseCase)
+
+const lessonRepository = new PrismaLessonRepository()
+const lessonUseCase = new LessonUseCase(lessonRepository)
+export const lessonController = new LessonController(lessonUseCase)
