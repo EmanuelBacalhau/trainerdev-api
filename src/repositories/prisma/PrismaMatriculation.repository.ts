e731c6ce1747425d852cbea59519bb607f2a4a7b
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-10-27 13:14:23 -0300

    feat(matriculation): feat(lesson): implement PrismaRepository for matriculation

---
+++ src/repositories/prisma/PrismaMatriculation.repository.ts
@@ -0,0 +1,109 @@
+import { prisma } from '../../databases/prisma'
+import type {
+  CreateMatriculationAttributes,
+  FindMatriculationResponse,
+  FindMatriculationsParams,
+  IMatriculationRepository,
+  Matriculation,
+  MatriculationWhereInputs,
+  UpdateMatriculationAttributes,
+} from '../IMatriculationRepository.interface'
+
+export class PrismaMatriculationRepository implements IMatriculationRepository {
+  find = async (
+    params: FindMatriculationsParams
+  ): Promise<FindMatriculationResponse[]> => {
+    const matriculations = await prisma.matriculation.findMany({
+      where: {
+        userId: params.where?.userId,
+        trackId: params.where?.trackId,
+        serialCode: {
+          contains: params.where?.serialCode?.contains,
+          mode: params.where?.serialCode?.mode,
+        },
+      },
+      orderBy: {
+        createdAt: params.order,
+      },
+      skip: params.skip,
+      take: params.take,
+      select: {
+        id: true,
+        serialCode: true,
+        status: true,
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          },
+        },
+        track: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    })
+
+    return matriculations
+  }
+
+  findById = (id: number): Promise<Matriculation | null> => {
+    return prisma.matriculation.findUnique({
+      where: {
+        id,
+      },
+    })
+  }
+
+  findByUserId = async (userId: number): Promise<Matriculation | null> => {
+    return prisma.matriculation.findFirst({
+      where: {
+        userId,
+      },
+    })
+  }
+
+  create = async (
+    attributes: CreateMatriculationAttributes
+  ): Promise<Matriculation> => {
+    return prisma.matriculation.create({
+      data: {
+        userId: attributes.userId,
+        trackId: attributes.trackId,
+        serialCode: attributes.serialCode,
+      },
+    })
+  }
+
+  update = async (
+    id: number,
+    attributes: UpdateMatriculationAttributes
+  ): Promise<Matriculation> => {
+    return prisma.matriculation.update({
+      where: {
+        id,
+      },
+      data: {
+        userId: attributes.userId,
+        trackId: attributes.trackId,
+      },
+    })
+  }
+
+  delete = async (id: number): Promise<void> => {
+    prisma.matriculation.delete({
+      where: {
+        id,
+      },
+    })
+  }
+
+  count = async (where: MatriculationWhereInputs): Promise<number> => {
+    return prisma.matriculation.count({
+      where,
+    })
+  }
+}
