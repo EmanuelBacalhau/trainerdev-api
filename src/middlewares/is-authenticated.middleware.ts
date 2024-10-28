4b1a88adf74325de0a4133a413eb163dab70b652
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-10-28 13:41:32 -0300

    feat(middleware): add is authenticated middleware

---
+++ src/middlewares/is-authenticated.middleware.ts
@@ -0,0 +1,34 @@
+import type { Handler } from 'express'
+import { verify } from 'jsonwebtoken'
+import { env } from '../env'
+import { HttpError } from '../errors/HttpError'
+
+interface JwtPayload {
+  role: string
+}
+
+export const isAuthenticatedMiddleware: Handler = (request, response, next) => {
+  try {
+    const authorization = request.headers.authorization
+
+    if (!authorization) {
+      throw new HttpError('Unauthorized', 401)
+    }
+
+    const [, token] = authorization.split(' ')
+
+    const decoded = verify(token, env.JWT_SECRET) as JwtPayload
+
+    if (!decoded) {
+      throw new HttpError('Unauthorized', 401)
+    }
+
+    request.user = {
+      role: decoded.role,
+    }
+
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,7 @@ import {
   userController,
 } from './container'
 import { upload } from './libs/multer'
+import { isAuthenticatedMiddleware } from './middlewares/is-authenticated.middleware'
 
 export const router = Router()
 
@@ -32,72 +33,116 @@ const lessonMulterConfig = multer({
 router.post('/session', authController.execute)
 
 // USERS
-router.get('/users', userController.index)
-router.get('/users/:id', userController.findById)
-router.post('/users', userController.create)
+router.get('/users', isAuthenticatedMiddleware, userController.index)
+router.get('/users/:id', isAuthenticatedMiddleware, userController.findById)
+router.post('/users', isAuthenticatedMiddleware, userController.create)
 router.put(
   '/users/:id',
+  isAuthenticatedMiddleware,
   userMulterConfig.single('avatar'),
   userController.update
 )
-router.delete('/users/:id', userController.delete)
+router.delete('/users/:id', isAuthenticatedMiddleware, userController.delete)
 
 // TRACKS
-router.get('/tracks', trackController.index)
-router.get('/tracks/:id', trackController.findById)
-router.get('/tracks/slug/:slug', trackController.findBySlug)
+router.get('/tracks', isAuthenticatedMiddleware, trackController.index)
+router.get('/tracks/:id', isAuthenticatedMiddleware, trackController.findById)
+router.get(
+  '/tracks/slug/:slug',
+  isAuthenticatedMiddleware,
+  trackController.findBySlug
+)
 router.post(
   '/tracks',
+  isAuthenticatedMiddleware,
   trackMulterConfig.single('cover'),
   trackController.create
 )
 router.put(
   '/tracks/:id',
+  isAuthenticatedMiddleware,
   trackMulterConfig.single('cover'),
   trackController.update
 )
-router.delete('/tracks/:id', trackController.delete)
+router.delete('/tracks/:id', isAuthenticatedMiddleware, trackController.delete)
 
 // MODULES
-router.get('/modules', moduleController.index)
-router.get('/modules/:id', moduleController.findById)
-router.get('/modules/slug/:slug', moduleController.findBySlug)
+router.get('/modules', isAuthenticatedMiddleware, moduleController.index)
+router.get('/modules/:id', isAuthenticatedMiddleware, moduleController.findById)
+router.get(
+  '/modules/slug/:slug',
+  isAuthenticatedMiddleware,
+  moduleController.findBySlug
+)
 router.post(
   '/modules',
+  isAuthenticatedMiddleware,
   moduleMulterConfig.single('cover'),
   moduleController.create
 )
 router.put(
   '/modules/:id',
+  isAuthenticatedMiddleware,
   moduleMulterConfig.single('cover'),
   moduleController.update
 )
-router.delete('/modules/:id', moduleController.delete)
+router.delete(
+  '/modules/:id',
+  isAuthenticatedMiddleware,
+  moduleController.delete
+)
 
 // LESSONS
-router.get('/lessons', lessonController.index)
-router.get('/lessons/:id', lessonController.findById)
-router.get('/lessons/slug/:slug', lessonController.findBySlug)
+router.get('/lessons', isAuthenticatedMiddleware, lessonController.index)
+router.get('/lessons/:id', isAuthenticatedMiddleware, lessonController.findById)
+router.get(
+  '/lessons/slug/:slug',
+  isAuthenticatedMiddleware,
+  lessonController.findBySlug
+)
 router.post(
   '/lessons',
+  isAuthenticatedMiddleware,
   lessonMulterConfig.single('video'),
   lessonController.create
 )
 router.put(
   '/lessons/:id',
+  isAuthenticatedMiddleware,
   lessonMulterConfig.single('video'),
   lessonController.update
 )
-router.delete('/lessons/:id', lessonController.delete)
+router.delete(
+  '/lessons/:id',
+  isAuthenticatedMiddleware,
+  lessonController.delete
+)
 
 // MATRICULATIONS
-router.get('/matriculations', matriculationController.index)
-router.post('/matriculations', matriculationController.create)
-router.put('/matriculations/:id', matriculationController.update)
-router.delete('/matriculations/:id', matriculationController.delete)
+router.get(
+  '/matriculations',
+  isAuthenticatedMiddleware,
+  matriculationController.index
+)
+router.post(
+  '/matriculations',
+  isAuthenticatedMiddleware,
+  matriculationController.create
+)
+router.put(
+  '/matriculations/:id',
+  isAuthenticatedMiddleware,
+  matriculationController.update
+)
+router.delete(
+  '/matriculations/:id',
+  isAuthenticatedMiddleware,
+  matriculationController.delete
+)
 
 // TRACK_MODULES
 router.delete(
   '/tracks/:id/modules/:moduleId',
+  isAuthenticatedMiddleware,
   trackController.removeModuleFromTrack
 )
