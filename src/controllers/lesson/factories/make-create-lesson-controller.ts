0ae800ae7393e33aacc6496cf32e1d60fbef0f16
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-10-31 20:57:05 -0300

    refactor(lesson): add factories in router

--- src/controllers/lesson/factories/make-create-lesson-controller.ts
+++ src/controllers/lesson/factories/make-create-lesson-controller.ts
@@ -1,7 +1,7 @@
 import { makeCreateLessonService } from '@/services/lesson/factories/make-create-lesson-service'
 import { CreateLessonController } from '../create-lesson.controller'
 
-export function maleCreateLessonController() {
+export function makeCreateLessonController() {
   const lessonService = makeCreateLessonService()
   const lessonController = new CreateLessonController(lessonService)
   return lessonController
--- src/routes/lesson.router.ts
+++ src/routes/lesson.router.ts
@@ -1,4 +1,9 @@
-import { lessonController } from '@/container'
+import { makeCreateLessonController } from '@/controllers/lesson/factories/make-create-lesson-controller'
+import { makeDeleteLessonController } from '@/controllers/lesson/factories/make-delete-lesson-controller'
+import { makeFindAllLessonController } from '@/controllers/lesson/factories/make-find-all-lesson-controller'
+import { makeFindLessonByIdController } from '@/controllers/lesson/factories/make-find-lesson-by-id-controller'
+import { makeFindLessonBySlugController } from '@/controllers/lesson/factories/make-find-lesson-by-slug-controller'
+import { makeUpdateLessonController } from '@/controllers/lesson/factories/make-update-lesson-controller'
 import { multerUpload } from '@/libs/multerUpload'
 import { isAuthenticatedMiddleware } from '@/middlewares/is-authenticated.middleware'
 import { verifyUserRole } from '@/middlewares/verify-user-role.middleware'
@@ -8,41 +13,48 @@ export const lessonRouter = Router()
 
 const lessonMulterConfig = multerUpload('lessons', ['video/mp4'])
 
+const findAllLessonController = makeFindAllLessonController()
+const createLessonController = makeCreateLessonController()
+const updateLessonController = makeUpdateLessonController()
+const findLessonByIdController = makeFindLessonByIdController()
+const findLessonBySlugController = makeFindLessonBySlugController()
+const deleteLessonController = makeDeleteLessonController()
+
 lessonRouter.get(
   '/lessons',
   isAuthenticatedMiddleware,
   verifyUserRole(['ADMIN']),
-  lessonController.index
+  findAllLessonController.execute.bind(findAllLessonController)
 )
 lessonRouter.get(
   '/lessons/:id',
   isAuthenticatedMiddleware,
   verifyUserRole(['ADMIN', 'TRAINEE', 'TRAINER']),
-  lessonController.findById
+  findLessonByIdController.execute.bind(findLessonByIdController)
 )
 lessonRouter.get(
   '/lessons/slug/:slug',
   isAuthenticatedMiddleware,
   verifyUserRole(['ADMIN', 'TRAINEE', 'TRAINER']),
-  lessonController.findBySlug
+  findLessonBySlugController.execute.bind(findLessonBySlugController)
 )
 lessonRouter.post(
   '/lessons',
   isAuthenticatedMiddleware,
   verifyUserRole(['ADMIN', 'TRAINER']),
   lessonMulterConfig.single('video'),
-  lessonController.create
+  createLessonController.execute.bind(createLessonController)
 )
 lessonRouter.put(
   '/lessons/:id',
   isAuthenticatedMiddleware,
   verifyUserRole(['ADMIN', 'TRAINER']),
   lessonMulterConfig.single('video'),
-  lessonController.update
+  updateLessonController.execute.bind(updateLessonController)
 )
 lessonRouter.delete(
   '/lessons/:id',
   isAuthenticatedMiddleware,
   verifyUserRole(['ADMIN', 'TRAINER']),
-  lessonController.delete
+  deleteLessonController.execute.bind(deleteLessonController)
 )
