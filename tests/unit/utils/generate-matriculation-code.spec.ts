e6b2dda4b1167228984cf0e3f8faa9cbd74d2a3b
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-11-01 10:42:07 -0300

    chore: remove tests

--- tests/unit/utils/generate-matriculation-code.spec.ts
+++
@@ -1,7 +0,0 @@
-import { generateMatriculationCode } from '@/utils/generate-matriculation-code'
-import { expect, test } from 'vitest'
-
-test('should be able to generate a matriculation code', () => {
-  const matriculationCode = generateMatriculationCode()
-  expect(matriculationCode).toEqual(expect.any(String))
-})
