4cbf551518cfcc97d2c6f53f64db1b2fbaee6063
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-10-31 22:53:27 -0300

    fix(module): correcting factory of find module by slug controller

--- src/controllers/module/factories/make-find-module-by-slug-controller.ts
+++ src/controllers/module/factories/make-find-module-by-slug-controller.ts
@@ -1,10 +1,10 @@
-import { makeFindModuleByIdService } from '@/services/module/factories/make-find-module-by-id-service'
-import { FindModuleByIdController } from '../find-module-by-id.controller'
+import { makeFindModuleBySlugService } from '@/services/module/factories/make-find-module-by-slug-service'
+import { FindModuleBySlugController } from '../find-module-by-slug.controller'
 
 export function makeFindModuleBySlugController() {
-  const findModuleByIdService = makeFindModuleByIdService()
-  const findModuleByIdController = new FindModuleByIdController(
-    findModuleByIdService
+  const findModuleBySlugService = makeFindModuleBySlugService()
+  const findModuleBySlugController = new FindModuleBySlugController(
+    findModuleBySlugService
   )
-  return findModuleByIdController
+  return findModuleBySlugController
 }
