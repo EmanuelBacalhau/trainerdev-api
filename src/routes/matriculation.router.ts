13eab318ebd58dc1db95b9cc1b8b2b8f449eb52c
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-10-31 22:38:37 -0300

    refactor(matriculation): add call factories controllers

--- src/routes/matriculation.router.ts
+++ src/routes/matriculation.router.ts
@@ -1,31 +1,39 @@
-import { matriculationController } from '@/container'
+import { makeCreateMatriculationController } from '@/controllers/matriculation/factories/make-create-matriculation-controller'
+import { makeDeleteMatriculationController } from '@/controllers/matriculation/factories/make-delete-matriculation-controller'
+import { makeFindAllMatriculationController } from '@/controllers/matriculation/factories/make-find-all-matriculation-controller'
+import { makeUpdateMatriculationController } from '@/controllers/matriculation/factories/make-update-matriculation-controller'
 import { isAuthenticatedMiddleware } from '@/middlewares/is-authenticated.middleware'
 import { verifyUserRole } from '@/middlewares/verify-user-role.middleware'
 import { Router } from 'express'
 
 export const matriculationRouter = Router()
 
+const findAllMatriculationController = makeFindAllMatriculationController()
+const createMatriculationController = makeCreateMatriculationController()
+const updateMatriculationController = makeUpdateMatriculationController()
+const deleteMatriculationController = makeDeleteMatriculationController()
+
 matriculationRouter.get(
   '/matriculations',
   isAuthenticatedMiddleware,
   verifyUserRole(['ADMIN']),
-  matriculationController.index
+  findAllMatriculationController.execute.bind(findAllMatriculationController)
 )
 matriculationRouter.post(
   '/matriculations',
   isAuthenticatedMiddleware,
   verifyUserRole(['ADMIN']),
-  matriculationController.create
+  createMatriculationController.execute.bind(createMatriculationController)
 )
 matriculationRouter.put(
   '/matriculations/:id',
   isAuthenticatedMiddleware,
   verifyUserRole(['ADMIN']),
-  matriculationController.update
+  updateMatriculationController.execute.bind(updateMatriculationController)
 )
 matriculationRouter.delete(
   '/matriculations/:id',
   isAuthenticatedMiddleware,
   verifyUserRole(['ADMIN']),
-  matriculationController.delete
+  deleteMatriculationController.execute.bind(deleteMatriculationController)
 )
