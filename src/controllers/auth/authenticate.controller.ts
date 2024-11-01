f975e4848c33fd9b0a99b8ec52c4e303cae42a3b
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-10-31 22:31:21 -0300

    refactor(auth): add single responsibility in controller

---
+++ src/controllers/auth/authenticate.controller.ts
@@ -0,0 +1,23 @@
+import type { AuthenticateService } from '@/services/auth/authenticate.service'
+import type { NextFunction, Request, Response } from 'express'
+import type { IController } from '../controller.interface'
+import { AuthRequestSchema } from '../schemas/auth-request-schema'
+
+export class AuthenticateController implements IController {
+  constructor(private readonly authenticateService: AuthenticateService) {}
+
+  async execute(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const body = AuthRequestSchema.parse(req.body)
+      const token = await this.authenticateService.execute(body)
+
+      res.status(200).json({ token })
+    } catch (error) {
+      next(error)
+    }
+  }
+}
